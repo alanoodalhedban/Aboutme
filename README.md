8fff14ffb3cff2443f173be80600a94ceab02ca5
alanoodalhedban <alanoodalhedban@gmail.com>
2021-05-24 16:10:09 +0300

    add questions

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Aboutme
+
+**my name is alanood alhedban  from maan ,I'm 24 year old , I graduated from Al-Hussein Bin Talal University  
+computer science  , I always strive to develop myself by taking courses in various fields ,I hope to become a professional  a web developer.**
+
+
+
+# User Stories:
+
+
+**I want to create web  site titled 'Healthy life' Found in the case of the idea because we live in conditions of COVID19 so that , we must maintain our health and our diet to obtain strong immunity .**
+
+**When the user opens the web, he must log in, and after that  he will be asked several questions
+Such as how old are you?, how height and weighed?, do you suffer from any health diseases?, what is your favorite food? , After that, a special diet for him is developed and provided with some information in order to have a healthy life.**
+
---
+++ js/app.js
@@ -0,0 +1,95 @@
+'use strict' ;
+
+let userage  = prompt('Is your age between(18-50)?');
+
+
+
+switch(userage.toLowerCase()) {
+    case 'yes' :
+    case  'y' :
+        // console.log('correct') 
+        alert('this is fine');
+        break;
+
+    case 'no' :
+    case  'n' :
+        // console.log('Incorrect') 
+       alert('This age is not suitable') ;
+       break;
+
+}
+
+let Uasck = prompt('Do you suffer from diseases?');
+ switch(Uasck.toLowerCase()) {
+    case 'yes' :
+    case  'y' :
+        // console.log('correct') 
+        alert('this is suitable');
+        break;
+
+    case 'no' :
+    case  'n' :
+        // console.log('Incorrect') 
+       alert('This age is not suitable') ;
+       break;
+
+}
+let Udrink = prompt('Do you drink coffee or tea?');
+ switch(Udrink.toLowerCase()) {
+    case 'yes' :
+    case  'y' :
+        // console.log('correct') 
+        alert('this is suitable');
+        break;
+
+    case 'no' :
+    case  'n' :
+        // console.log('Incorrect') 
+       alert('This age is not suitable') ;
+       break;
+
+}
+
+let Ubreak = prompt('Do you eat breakfast early?');
+ switch(Ubreak.toLowerCase()) {
+    case 'yes' :
+    case  'y' :
+        // console.log('correct') 
+        alert('this is suitable');
+        break;
+
+    case 'no' :
+    case  'n' :
+        // console.log('Incorrect') 
+       alert('This age is not suitable') ;
+       break;
+ }
+
+ let Uweight = prompt('Do you weigh between 45-60?');
+ switch( Uweight.toLowerCase()) {
+    case 'yes' :
+    case  'y' :
+        // console.log('correct') 
+        alert('this is suitable');
+        break;
+
+    case 'no' :
+    case  'n' :
+        // console.log('Incorrect') 
+       alert('This age is not suitable') ;
+       break;
+ }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
