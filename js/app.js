71241d41e6b00decdeb77c04cb943a7ff58b225b
alanoodalhedban <alanoodalhedban@gmail.com>
2021-05-25 23:40:59 +0300

    add notes

--- js/app.js
+++ js/app.js
@@ -7,13 +7,13 @@ let Userage  = prompt('Is your age between(18-50)y?');
 switch(Userage.toLowerCase()) {
 case 'yes' :
 case  'y' :
-  // console.log('correct') 
+  // console.log('correct')
   alert('this is fine');
   break;
 
 case 'no' :
 case  'n' :
-  // console.log('Incorrect') 
+  // console.log('Incorrect')
   alert('This age is not suitable') ;
   break;
 
@@ -23,13 +23,13 @@ let Uasck = prompt('Do you suffer from diseases?');
 switch(Uasck.toLowerCase()) {
 case 'yes' :
 case  'y' :
-  // console.log('correct') 
+  // console.log('correct')
   alert('this is suitable');
   break;
 
 case 'no' :
 case  'n' :
-  // console.log('Incorrect') 
+  // console.log('Incorrect')
   alert('This age is not suitable') ;
   break;
 
@@ -38,13 +38,13 @@ let Udrink = prompt('Do you drink coffee or tea?');
 switch(Udrink.toLowerCase()) {
 case 'yes' :
 case  'y' :
-  // console.log('correct') 
+  // console.log('correct')
   alert('this is suitable');
   break;
 
 case 'no' :
 case  'n' :
-  // console.log('Incorrect') 
+  // console.log('Incorrect')
   alert('This age is not suitable') ;
   break;
 
@@ -54,13 +54,13 @@ let Ubreak = prompt('Do you eat breakfast early?');
 switch(Ubreak.toLowerCase()) {
 case 'yes' :
 case  'y' :
-  // console.log('correct') 
+  // console.log('correct')
   alert('this is suitable');
   break;
 
 case 'no' :
 case  'n' :
-  // console.log('Incorrect') 
+  // console.log('Incorrect')
   alert('This age is not suitable') ;
   break;
 }
@@ -69,13 +69,13 @@ let Uweight = prompt('Do you weigh between 45-60?');
 switch( Uweight.toLowerCase()) {
 case 'yes' :
 case  'y' :
-  // console.log('correct') 
+  // console.log('correct')
   alert('this is suitable');
   break;
 
 case 'no' :
 case  'n' :
-  // console.log('Incorrect') 
+  // console.log('Incorrect')
   alert('This age is not suitable') ;
   break;
 }
@@ -100,26 +100,49 @@ for(let i=0;i<=4;i++){
   }else {
     alert('The correct Answer is 4') ;
   }
-    
+
 }
 
 let score =0 ;
-let Bfood =prompt('what is healthy food?') ;
-let Bestfood =['appl','fruit','orange','Vegetables'];
+
+let Bestfood =['apple','chiken','coffe','choclate','Pepsi','meat'];
+// console.log(Bestfood);
 for(let i=0;i<Bestfood.length;i++){
+  let Bfood =prompt('what is  the example of healthy food?') ;
 
-  if(Bfood===Bestfood[i])
-  {
-    alert('correct'  +  Bestfood[i] );
+  if(Bfood===Bestfood[0]){
+    alert('That is right' );
     score ++ ;
-    
     alert(`Your score is  ${score}`);
-   
-  } 
-  
-       
-        
+    score ++ ;
+    break;
 
+  }
+  else if (Bfood===Bestfood[1]){
+    Bfood=prompt('wrong guess');
+
+  }
+  else if (Bfood===Bestfood[2]){
+    Bfood==prompt('wrong guess');
+  }
+  else if (Bfood===Bestfood[3]){
+    Bfood==prompt('wrong guess');
+  }
+  else if (Bfood===Bestfood[4]){
+    Bfood==prompt('wrong guess');
+  }
+  else if (Bfood===Bestfood[5]){
+    Bfood==prompt('wrong guess');
+  }
+  else 
+  {  
+    
+    alert('the example of healthy food is'+  Bestfood[0]);
+    alert(`Your score is  ${score}`);
+
+  }
+
+ 
  
 }
 
